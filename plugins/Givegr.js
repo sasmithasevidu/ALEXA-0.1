b4a88a17718256587bdac3ef3bc000fa76a1455e
ChamodKeshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-03-10 07:48:27 +0530

    Update give qr

---
+++ plugins/Givegr.js
@@ -0,0 +1,11 @@
+// Plugin for raganork by souravkl11
+var ra = require('../events');// Plugin for raganork by souravkl11
+var ra = require('../events');
+var {MessageType, Mimetype} = require('@adiwajshing/baileys');
+var axios = require('axios');
+ra.addCommand({pattern: 'getqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
+await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://baileys-qr.herokuapp.com/api/raganork-qr'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)You can only get session using this code \n 2) QR is only valid for 20 seconds"})});
+var {MessageType, Mimetype} = require('@adiwajshing/baileys');
+var axios = require('axios');
+ra.addCommand({pattern: 'getqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
+await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://replit.com/@tenuh/Alexa?v=1'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)You can only get session using this code \n 2) QR is only valid for 20 seconds"})});
