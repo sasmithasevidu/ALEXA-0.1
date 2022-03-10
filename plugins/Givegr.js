f47036f1743d784aa990714dcdb073aecd58ef55
ChamodKeshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-03-10 08:01:47 +0530

    Fixed error

--- plugins/Givegr.js
+++ plugins/Givegr.js
@@ -5,7 +5,3 @@ var {MessageType, Mimetype} = require('@adiwajshing/baileys');
 var axios = require('axios');
 ra.addCommand({pattern: 'getqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
 await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://replit.com/@tenuh/Alexa?v=1'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)You can only get session using this code \n 2) QR is only valid for 20 seconds"})});
-var {MessageType, Mimetype} = require('@adiwajshing/baileys');
-var axios = require('axios');
-ra.addCommand({pattern: 'getqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
-await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://replit.com/@tenuh/Alexa?v=1'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)You can only get session using this code \n 2) QR is only valid for 20 seconds"})});
