584d1d537524c13d69355524d47cda949c70e601
ChamodKeshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-03-27 09:14:34 +0530

    Update and rename alexa_new.js to plugins/alexa_new.js

--- plugins/alexa_new.js
+++ plugins/alexa_new.js
@@ -1,9 +1,7 @@
-/* Codded by @Ravindu Manoj
-Telegram: t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
+/* Codded by Kavindya-X
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Whats bot - Ravindu Manoj
+Alexa- Kavindya-X
 */
 
 const fs = require('fs')
