6070b4373444c7aa2e594ad459c0c09133a1d61a
ChamodKeshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-03-29 18:41:51 +0530

    Update Fb.js

--- plugins/Fb.js
+++ plugins/Fb.js
@@ -1,8 +1,19 @@
-let xfar = require('xfarr-api')
-let fetch = require('node-fetch')
+const amazone = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const Config = require('../config');
+const tenu = require('../hatzu');
+const Language = require('../language');
+const Lang = Language.getString('amazone');
+const YTV_DESC = "Facebook Video Downloader V2 ."
+const YT_NEED = "*need word!.*"
+const DWLOAD_VID = "*🎭Downloading Your Video...*"
+const YTV_UP = "*🚀Uploading Your Video...*"
+const NO_RESULT = "*🌀can't Find Anything...*"
+const tk = Config.WORKTYPE == 'public' ? false : true
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw 'Masukkan Link\n\nContoh: .fb https://facebook.com/xxxxxx'
-  let res = await xfar.Facebook(text)
 m.reply('*Tunggu Sebentar...*')
 conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
 Link Video: ${res.url}
